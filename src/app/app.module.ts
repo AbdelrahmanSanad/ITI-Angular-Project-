59f0aa48501a56c625ad0af0b847bb542582c36d
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-05-24 10:31:50 +0300

    implements the ui of user info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import{HttpClientModule} from '@angular/common/http';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
