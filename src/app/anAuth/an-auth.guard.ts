9a0a1e8533c9108a8283c7daa9beba7fd41ece36
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-05-27 15:38:56 +0300

    handling errors

--- src/app/anAuth/an-auth.guard.ts
+++ src/app/anAuth/an-auth.guard.ts
@@ -5,7 +5,6 @@ import {
   RouterStateSnapshot,
   Router,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthService } from '../Services/routing-guard.service';
 @Injectable({
   providedIn: 'root',
